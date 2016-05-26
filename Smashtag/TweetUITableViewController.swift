255a8084fd2b5ce5915787bb0dca8acd69f11d72
muxing1991 <muxing19910807@hotmail.com>
2016-05-26 20:02:46 +0800

    初步搭建

---
+++ Smashtag/TweetUITableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TweetUITableViewController.swift
+//  Smashtag
+//
+//  Created by 杨威 on 16/5/26.
+//  Copyright © 2016年 demo. All rights reserved.
+//
+
+import UIKit
+
+class TweetUITableViewController: UITableViewController {
+  
+  // model 
+  var tweets = [[Tweet]]()
+  
+  
+}
